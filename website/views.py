a568df57783112cf5978b7d3b26fa5c515c85c03
ananya0809 <66442410+ananya0809@users.noreply.github.com>
2022-03-22 22:01:23 +0530

    added POST and GET requests for notes addition and deletion

--- website/views.py
+++ website/views.py
@@ -1,11 +1,38 @@
 #make home page for website
 
-from flask import Blueprint, render_template #it has a bunch of urls/roots inside of it
+from flask import Blueprint, render_template, request, flash, jsonify  # it has a bunch of urls/roots inside of it
+from flask_login import login_required, current_user
+from .models import Note
+from . import db
+import json
 
 views = Blueprint('views',__name__)
 
-@views.route('/') #whenever we go to home(/) the home function will run
+@views.route('/', methods=['GET', 'POST']) #whenever we go to home(/) the home function will run
+@login_required
 def home():
-    return render_template("home.html")
+    if request.method == 'POST':
+        note = request.form.get('note')
+
+        if len(note) < 1:
+            flash('Note is too short!', category='error')
+        else:
+            new_note = Note(data=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash('Note created!', category='success')
+
+    return render_template("home.html", user=current_user)
+@views.route('/delete-note', methods=['POST'])
+def delete_note():
+    note = json.loads(request.data)
+    noteId = note['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+
+    return jsonify({})
 
 
